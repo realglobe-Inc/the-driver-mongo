d4b38b4b74ec6ee8c55aed81da5bd3aa743bde33
okunishinishi <okunishinishi@gmail.com>
2018-06-04 12:54:56 +0900

    Initial commit

---
+++ test/TheDriverMongoTest.js
@@ -0,0 +1,22 @@
+/**
+ * Test for TheDriverMongo.
+ * Runs with mocha.
+ */
+'use strict'
+
+const TheDriverMongo = require('../lib/TheDriverMongo')
+const { ok, equal } = require('assert')
+
+describe('the-driver-mongo', () => {
+  before(() => {
+  })
+
+  after(() => {
+  })
+
+  it('Do test', () => {
+    ok(TheDriverMongo)
+  })
+})
+
+/* global describe, before, after, it */
---
+++ test/mongoMixTest.js
@@ -0,0 +1,22 @@
+/**
+ * Test for mongoMix.
+ * Runs with mocha.
+ */
+'use strict'
+
+const mongoMix = require('../lib/mixins/mongoMix')
+const { ok, equal } = require('assert')
+
+describe('mongo-mix', () => {
+  before(() => {
+  })
+
+  after(() => {
+  })
+
+  it('Do test', () => {
+
+  })
+})
+
+/* global describe, before, after, it */
