83b3b10cfacf0461d9f93f705369f118cc749c07
okunishinishi <okunishinishi@gmail.com>
2018-06-04 14:54:35 +0900

    Version incremented to 1.0.1

---
+++ test/parserTest.js
@@ -0,0 +1,22 @@
+/**
+ * Test for parser.
+ * Runs with mocha.
+ */
+'use strict'
+
+const parser = require('../lib/helpers/parser')
+const { ok, equal } = require('assert')
+
+describe('parser', () => {
+  before(() => {
+  })
+
+  after(() => {
+  })
+
+  it('Do test', () => {
+
+  })
+})
+
+/* global describe, before, after, it */
